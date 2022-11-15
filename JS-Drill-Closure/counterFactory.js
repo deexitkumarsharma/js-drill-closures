9f12a1d87aa24ee7b91389cf8feca67e8fdb9923
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-11-15 11:53:05 +0000

    Update JS-Drill-Closure/counterFactory.js, JS-Drill-Closure/cacheFunction.js, JS-Drill-Closure/limitFunctionCallCount.js

--- JS-Drill-Closure/counterFactory.js
+++ JS-Drill-Closure/counterFactory.js
@@ -1,3 +1,7 @@
+    // Return an object that has two methods called `increment` and `decrement`.
+    // `increment` should increment a counter variable in closure scope and return it.
+    // `decrement` should decrement the counter variable and return it.
+    
 function counterFactory(counter){
 
     return {
--- JS-Drill-Closure/limitFunctionCallCount.js
+++ JS-Drill-Closure/limitFunctionCallCount.js
@@ -1,3 +1,7 @@
+    // Should return a function that invokes `cb`.
+    // The returned function should only allow `cb` to be invoked `n` times.
+    // Returning null is acceptable if cb can't be returned
+
 function limitFunctionCallCount(cb, n){
     let count = 0;
     return function invokeCb() { //fn helping to invoke cb
